16059584148284ee1958080944f3e988828510d1
primedildaar2 <primedildaar2@gmail.com>
2024-01-18 15:04:14 +0200

    added a post route for the api that I'm going to test

--- server.js
+++ server.js
@@ -2,6 +2,75 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const allCheckoutProducts = [];
+const allShopProducts = [
+  {
+    productName: "banana",
+    productCategory: "fruit",
+    price: 10,
+    quantity: 12,
+    content: "fresh, delicious,and potasium goodness",
+    Comments: [
+      { username: "dildaar", content: "they have great quality bananas" },
+    ],
+  },
+  {
+    productName: "apple",
+    productCategory: "fruit",
+    price: 5,
+    quantity: 12,
+    content:
+      "fresh, delicious,granny smith golden apples and a source of cyanide for people",
+    Comments: [
+      { username: "dildaar", content: "they have great quality apples" },
+    ],
+  },
+  {
+    productName: "peach",
+    productCategory: "fruit",
+    price: 15,
+    quantity: 12,
+    content: "fresh, delicious,soft peaches and they're divine",
+    Comments: [
+      { username: "dildaar", content: "they have great quality peaches" },
+    ],
+  },
+  {
+    productName: "pear",
+    productCategory: "fruit",
+    price: 10,
+    quantity: 12,
+    content: "fresh, delicious,soft pears get it before the other guy ",
+    Comments: [
+      { username: "dildaar", content: "they have great quality oranges" },
+    ],
+  },
+  {
+    productName: "orange",
+    productCategory: "pear",
+    price: 5,
+    quantity: 12,
+    content:
+      "fresh, delicious,and the practicaly the only fruit on this list that is a color",
+    Comments: [
+      { username: "dildaar", content: "they have great quality pears" },
+    ],
+  },
+  {
+    productName: "strawberry",
+    productCategory: "fruit",
+    price: 15,
+    quantity: 12,
+    content:
+      "fresh, delicious,strawberries, and a nice fruit in general that is or isn't used in strawberry flavoured products",
+    Comments: [
+      {
+        username: "dildaar",
+        content: "they have great quality strawberies",
+      },
+    ],
+  },
+];
 // add CORS middleware //
 // routes and data //
 
@@ -9,72 +78,11 @@ app.get("/", (req, res) => {
   res.send('<p style="background-color:black;color:white">Hello World!</p>');
 });
 app.get("/api/products", (req, res) => {
-  res.send([
-    {
-      productName: "banana",
-      productCategory: "fruit",
-      price: 10,
-      quantity: 12,
-      content:"fresh, delicious,and potasium goodness",
-      Comments: [
-        { username: "dildaar", content: "they have great quality bananas" },
-      ],
-    },
-    {
-      productName: "apple",
-      productCategory: "fruit",
-      price: 5,
-      quantity: 12,
-      content:"fresh, delicious,granny smith golden apples and a source of cyanide for people",
-      Comments: [
-        { username: "dildaar", content: "they have great quality apples" },
-      ],
-    },
-    {
-      productName: "peach",
-      productCategory: "fruit",
-      price: 15,
-      quantity: 12,
-      content:"fresh, delicious,soft peaches and they're divine",
-      Comments: [
-        { username: "dildaar", content: "they have great quality peaches" },
-      ],
-    },
-    {
-      productName: "pear",
-      productCategory: "fruit",
-      price: 10,
-      quantity: 12,
-      content:"fresh, delicious,soft pears get it before the other guy ",
-      Comments: [
-        { username: "dildaar", content: "they have great quality oranges" },
-      ],
-    },
-    {
-      productName: "orange",
-      productCategory: "pear",
-      price: 5,
-      quantity: 12,
-      content:"fresh, delicious,and the practicaly the only fruit on this list that is a color",
-      Comments: [
-        { username: "dildaar", content: "they have great quality pears" },
-      ],
-    },
-    {
-      productName: "strawberry",
-      productCategory: "fruit",
-      price: 15,
-      quantity: 12,
-      content:"fresh, delicious,strawberries, and a nice fruit in general that is or isn't used in strawberry flavoured products",
-      Comments: [
-        {
-          username: "dildaar",
-          content: "they have great quality strawberies",
-        },
-      ],
-    },
-  ]);
+  res.send(allShopProducts);
 });
+app.post("/api/allCheckoutProducts",(req,res) => {
+  res.send("Product Added to checkout");
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
